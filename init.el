f80eda3a37b395ffbd99a914c79b6b014dfcf973
ChenJie <chenjie1@sensetime.com>
2021-05-12 18:55:43 +0800

    update

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; load emacs 24's package system. Add MELPA repository.
 (define-key special-event-map [config-changed-event] #'ignore)
 
+;; unset C-m, seperate it with the RET key
+(define-key input-decode-map [?\C-m] [C-m])
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -15,6 +18,10 @@
 (setq  x-meta-keysym 'super
        x-super-keysym 'meta)
 
+(when (eq system-type 'darwin)
+  (setq mac-option-modifier 'super
+        mac-command-modifier 'meta))
+
 
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
@@ -26,6 +33,16 @@
 
 
 
+(use-package lsp-mode
+   :init
+   (setq
+    lsp-keymap-prefix "s-i"
+    lsp-ui-doc-enable nil
+    ))
+(use-package lsp-ui)
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,11 +50,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(expand-region easy-kill multiple-cursors powerline projectile evil-easymotion evil-collection evil helm-rg helm-ag use-package helm fzf neotree spacemacs-theme sublime-themes company lsp-mode golden-ratio-scroll-screen go-mode))
+   '(lsp-ui treemacs neotree expand-region easy-kill multiple-cursors powerline projectile evil-easymotion evil-collection evil helm-rg helm-ag use-package helm fzf spacemacs-theme sublime-themes company lsp-mode golden-ratio-scroll-screen go-mode))
  '(spacemacs-theme-custom-colors '((bg1 . "#171421"))))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'spacemacs-dark t)
+
 
 
 
@@ -52,9 +71,6 @@
           (tab-mark 9 [707 9] [92 9]) ; tab
           ))
 
-
-(global-set-key [f8] 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
 
 (add-hook 'prog-mode-hook 'linum-mode)
@@ -79,11 +95,6 @@
   (next-line)
   (recenter-top-bottom 0)
   )
-
-
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-i"))
 
 
 
@@ -134,21 +145,43 @@
   (unless (eq (window-end) (point-max))
     (scroll-up n)))
 
+;; scroll with cursor not move
 (global-set-key "\M-n" 'scroll-up-in-place)
 (global-set-key "\M-p" 'scroll-down-in-place)
+
+
+;; (global-set-key (kbd "M-c") 'kill-ring-save)
+;; (global-set-key (kbd "M-v") 'yank)
+;; (global-set-key (kbd "s-<backspace>") 'backward-kill-word)
+;; (global-set-key (kbd "M-k") 'kill-region)
+;; (global-set-key (kbd "C-j") 'save-buffer)
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-beginning-position 2)))))
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+
+
+
+
+;; scroll with C-m  C-,
+(global-set-key (kbd "<C-m>") 'golden-ratio-scroll-screen-up)
+(global-set-key (kbd "C-,") 'golden-ratio-scroll-screen-down)
 
 ;; (global-set-key (kbd "C-:") 'avy-goto-char)
 (global-set-key (kbd "M-;") 'avy-goto-word-0)
 
-(setq-default cursor-type 'bar) 
-(set-cursor-color "#00ff00") 
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
+(setq-default cursor-type 'bar)
+(set-cursor-color "#00ff00")
 
 
 (require 'multiple-cursors)
@@ -159,3 +192,77 @@
 
 
 
+
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+
+;; (require 'neotree)
+;; (global-set-key [f8] 'neotree-toggle)
+;; (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
