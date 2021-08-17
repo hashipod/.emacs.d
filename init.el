dd3b688824c071fd1ba43c8f76a93af10f510964
ChenJie <chenjie1@sensetime.com>
2021-08-17 17:28:55 +0800

    update c

--- init.el
+++ init.el
@@ -217,6 +217,23 @@
 
 
 
+(defun my-c-mode-common-hook ()
+ ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+ (c-set-offset 'substatement-open 0)
+ ;; other customizations can go here
+
+ (setq c++-tab-always-indent t)
+ (setq c-basic-offset 4)                  ;; Default is 2
+ (setq c-indent-level 4)                  ;; Default is 2
+
+ (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+ (setq tab-width 4)
+ (setq indent-tabs-mode t)  ; use spaces only if nil
+ )
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+
+
 (defun indent-between-pair (&rest _ignored)
   (newline)
   (indent-according-to-mode)
