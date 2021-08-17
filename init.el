683711e4975d6c3d0d925046ebb6793fe0e0528b
ChenJie <chenjie1@sensetime.com>
2021-08-17 17:37:04 +0800

    update c

--- init.el
+++ init.el
@@ -223,11 +223,11 @@
  ;; other customizations can go here
 
  (setq c++-tab-always-indent t)
- (setq c-basic-offset 4)                  ;; Default is 2
- (setq c-indent-level 4)                  ;; Default is 2
+ (setq c-basic-offset 8)                  ;; Default is 2
+ (setq c-indent-level 8)                  ;; Default is 2
 
- (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
- (setq tab-width 4)
+ (setq tab-stop-list '(8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+ (setq tab-width 8)
  (setq indent-tabs-mode t)  ; use spaces only if nil
  )
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
