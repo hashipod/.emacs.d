ba87b9ed5fccd19018de0c98480f86a19cc84a2e
ChenJie <chenjie1@sensetime.com>
2021-10-22 11:23:59 +0800

    fix nil

--- init.el
+++ init.el
@@ -48,13 +48,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "nil"))))
  '(evil-mc-cursor-default-face ((t (:inherit cursor :background "firebrick1" :inverse-video nil))))
  '(hydra-face-red ((t (:foreground "chocolate" :weight bold))))
- '(line-number ((t (:background "nil"))))
- '(line-number-current-line ((t (:background "nil")))))
+ )
 
 
+
+(unless (display-graphic-p)
+  (set-face-attribute 'default nil :background "nil")
+  (set-face-attribute 'line-number nil :background "nil")
+  (set-face-attribute 'line-number-current-line nil :background "nil")
+)
 
 
 
