69e5896307954dc71cbb97b2dec734f3e3638c67
ChenJie <chenjie1@sensetime.com>
2022-03-16 16:06:20 +0800

    update

--- init.el
+++ init.el
@@ -25,6 +25,32 @@
 ;;    (setq mac-option-modifier 'super
 ;;          mac-command-modifier 'meta))
 
+
+
+
+
+
+;;;;;; catch ESC in terminal(-nw) ;;;;;;;;;;;;
+(defvar personal/fast-keyseq-timeout 50)
+(defun personal/-tty-ESC-filter (map)
+  (if (and (equal (this-single-command-keys) [?\e])
+           (sit-for (/ personal/fast-keyseq-timeout 1000.0)))
+      [escape] map))
+(defun personal/-lookup-key (map key)
+  (catch 'found
+    (map-keymap (lambda (k b) (if (equal key k) (throw 'found b))) map)))
+(defun personal/catch-tty-ESC ()
+  "Setup key mappings of current terminal to turn a tty's ESC into `escape'."
+  (when (memq (terminal-live-p (frame-terminal)) '(t pc))
+    (let ((esc-binding (personal/-lookup-key input-decode-map ?\e)))
+      (define-key input-decode-map
+        [?\e] `(menu-item "" ,esc-binding :filter personal/-tty-ESC-filter)))))
+(personal/catch-tty-ESC)
+
+
+
+
+
 (setq visible-bell t)
 (setq ring-bell-function #'ignore)
 
@@ -629,7 +655,7 @@
       (god-mode-all))
   (keyboard-quit)
 )
-(global-set-key (kbd "C-c C-c") 'my-god-mode)
+(global-set-key (kbd "<escape>") 'my-god-mode)
 (god-mode-all) ; god-mode by default
 
 (defun my-god-below-newline-and-insert-mode()
